921d8801c8a77579bd8deac1e7a11327bc47c739
LauraAmaya08 <laurasofiaamayacasado@gmail.com>
2024-10-22 09:43:12 -0500

    feat: :sparkles: Funciones añadidas, cambio en archivo dml

--- dql_funciones.sql
+++ dql_funciones.sql
@@ -27,52 +27,37 @@ RETURN promedio_stock;
 END//
 DELIMITER ;
 
-
--- 3. Calcular el descuento aplicado a una compra.
+-- 3. Calcular el precio promedio de productos.
 
 DELIMITER //
-CREATE FUNCTION descuento_total(tipo_cliente_id INT, precio DECIMAL (10,2))
-RETURNS DECIMAL (10,2)
+CREATE FUNCTION promedio_precio()
+RETURNS DECIMAL(10,2)
 DETERMINISTIC
 BEGIN
-DECLARE descuento_total DECIMAL (10,2) DEFAULT 0;
-SELECT descuento INTO descuento_total FROM tipo_cliente
-WHERE id_tipo = tipo_cliente_id;
-RETURN precio - descuento_total;
+    DECLARE promedio DECIMAL(10,2);
+    SELECT avg(precio_unitario) INTO promedio
+    FROM producto;
+    RETURN promedio;
 END//
 DELIMITER ;
 
 -- 4. Determinar si un cultivo es rentable.
 
 
-
--- 5. Obtener el estado promedio de los animales.
+-- 5. Obtener funciones por categoría especifica.
 
 DELIMITER //
-CREATE FUNCTION estado_promedio_animales()
-RETURNS VARCHAR(50)
+CREATE FUNCTION funciones_categoria(id_categoria_e INT)
+RETURNS INT 
 DETERMINISTIC
 BEGIN
-    DECLARE exc_count INT DEFAULT 0;
-    DECLARE reg_count INT DEFAULT 0;
-    DECLARE mal_count INT DEFAULT 0;
-    DECLARE estado_promedio VARCHAR(10);
-	SELECT 
-        COUNT(CASE WHEN estado = 'Excelente' THEN 1 END),
-        COUNT(CASE WHEN estado = 'Regular' THEN 1 END),
-        COUNT(CASE WHEN estado = 'Malo' THEN 1 END)
-	INTO exc_count, reg_count, mal_count
-    FROM animal;
-    IF exc_count >= reg_count AND exc_count >= mal_count THEN
-        SET estado_promedio = 'Excelente';
-    ELSEIF reg_count >= exc_count AND reg_count >= mal_count THEN
-        SET estado_promedio = 'Regular';
-    ELSE
-        SET estado_promedio = 'Malo';
-    END IF;
-    RETURN estado_promedio;
+    DECLARE cantidad INT;
+    SELECT count(id_funcion) INTO cantidad
+    FROM funciones
+    WHERE id_categoria = id_categoria_e;
+    RETURN cantidad;
 END//
-DELIMITER ;
+DELIMITER ; 
 
 -- 6. Calcular el costo total de mantenimiento de tecnologías.
 
@@ -105,11 +90,37 @@ DELIMITER ;
 -- 8. Calcular el total de productos vendidos por un empleado específico.
 
 
+
 -- 9. Obtener el promedio de peso de los animales en un hábitat.
 
+DELIMITER //
+CREATE FUNCTION promedio_peso_habitat(id_habitat_e INT)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+DECLARE promedio_peso DECIMAL(10,2)  DEFAULT 0;
+SELECT avg(a.peso) INTO promedio_peso FROM animal a JOIN animales_habitat ah on a.id_animal = ah.id_animal
+WHERE ah.id_habitat = id_habitat_e;
+RETURN promedio_peso;
+END//
+DELIMITER ;
 
+-- 10. Calcular la cantidad de insumos necesarios para un producto específico.
 
--- 10. Calcular el total de insumos necesarios para un cultivo específico.
+DELIMITER //
+CREATE FUNCTION insumos_total_producto(producto_id_e INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+DECLARE insumos_total INT  DEFAULT 0;
+SELECT count(ip.id_insumo) INTO insumos_total 
+FROM insumos_producto ip 
+JOIN insumos i on i.id_insumo = ip.id_insumo
+JOIN producto p on p.id_producto = ip.id_producto
+WHERE ip.id_producto = producto_id_e;
+RETURN insumos_total;
+END//
+DELIMITER ;
 
 -- 11. Obtener el total de ventas en un rango de fechas.
 
@@ -126,6 +137,21 @@ DELIMITER ;
 
 
 -- 12. Calcular el tiempo promedio de entrega de las ventas.
+
+DELIMITER //
+CREATE FUNCTION tiempo_entrega()
+RETURNS VARCHAR(50)
+DETERMINISTIC
+BEGIN
+    DECLARE promedio_entrega DECIMAL(10,2);
+    
+    SELECT AVG(DATEDIFF(ev.fecha, v.fecha)) INTO promedio_entrega
+    FROM venta v JOIN 
+    entrega_venta ev
+    on v.id_venta = ev.id_venta;
+    RETURN concat(promedio_entrega, ' dias');
+END//
+DELIMITER ;
 
 
 -- 13. Calcular la cantidad de horas diarias de trabajo de un empleado.
@@ -189,4 +215,67 @@ DELIMITER ;
 
 
 -- 17. Calcular el porcentaje de clientes que compraron productos en estado excelente.
+
+
+-- 18. Calcular promedio de animales por habitat.
+
+DELIMITER //
+CREATE FUNCTION animales_habitat(id_habitat_e INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE cantidad INT DEFAULT 0;
+    SELECT COUNT(a.id_animal) INTO cantidad
+    FROM animal a
+    JOIN animales_habitat ah ON a.id_animal = ah.id_animal
+    WHERE ah.id_habitat = id_habitat_e;
+
+    RETURN cantidad;
+END//
+DELIMITER ;
+
+
+-- 19. Calcular el descuento aplicado a una compra.
+
+DELIMITER //
+CREATE FUNCTION descuento_total(tipo_cliente_id INT, precio DECIMAL (10,2))
+RETURNS DECIMAL (10,2)
+DETERMINISTIC
+BEGIN
+DECLARE descuento_total DECIMAL (10,2) DEFAULT 0;
+SELECT descuento INTO descuento_total FROM tipo_cliente
+WHERE id_tipo = tipo_cliente_id;
+RETURN precio - descuento_total;
+END//
+DELIMITER ;
+
+
+-- 20. Obtener el estado promedio de los animales.
+
+DELIMITER //
+CREATE FUNCTION estado_promedio_animales()
+RETURNS VARCHAR(50)
+DETERMINISTIC
+BEGIN
+    DECLARE exc_count INT DEFAULT 0;
+    DECLARE reg_count INT DEFAULT 0;
+    DECLARE mal_count INT DEFAULT 0;
+    DECLARE estado_promedio VARCHAR(10);
+	SELECT 
+        COUNT(CASE WHEN estado = 'Excelente' THEN 1 END),
+        COUNT(CASE WHEN estado = 'Regular' THEN 1 END),
+        COUNT(CASE WHEN estado = 'Malo' THEN 1 END)
+	INTO exc_count, reg_count, mal_count
+    FROM animal;
+    IF exc_count >= reg_count AND exc_count >= mal_count THEN
+        SET estado_promedio = 'Excelente';
+    ELSEIF reg_count >= exc_count AND reg_count >= mal_count THEN
+        SET estado_promedio = 'Regular';
+    ELSE
+        SET estado_promedio = 'Malo';
+    END IF;
+    RETURN estado_promedio;
+END//
+DELIMITER ;
+
 
